1237654fa0384ed4d778199c5cab5e407e886cfa
agnoam <agnoam.aghai.ooo.123@gmail.com>
2019-11-11 00:28:36 +0200

    figure out how to make http requests

---
+++ Assets/Scripts/HttpServices.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpServices {
+
+    private const string serverURL = "http://example.server.com";
+    
+    
+    // TODO: Add Google ads to win coins
+
+    // Making http requests to checkout about coins
+    public static async void exampleHttpPostReq() {
+        string stringifiedJson = "{}";
+
+        // Making async POST http request by unity package
+        UnityWebRequest req = UnityWebRequest.Post($"{serverURL}/postExample", stringifiedJson);
+        UnityWebRequestAsyncOperation operation = req.SendWebRequest();
+        operation.completed += (AsyncOperation op) => {
+            // Runs when the request done
+        };
+    }
+
+    public static void exampleHttpGetReq() {
+        // Making the POST http request by unity package
+        UnityWebRequest req = UnityWebRequest.Get($"{serverURL}/getExample");
+        UnityWebRequestAsyncOperation operation = req.SendWebRequest();
+        operation.completed += (AsyncOperation op) => {
+            // Runs when the request done
+        };
+    }
+}
